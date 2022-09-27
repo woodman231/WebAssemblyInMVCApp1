49ab01e15c557ba5285173c1a0968c4fccdb2203
Sean Woodward <swoodward@intertech.com>
2022-09-27 11:10:28 -0500

    Add project files.

---
+++ WebAssemblyInMVCApp1/Controllers/ComponentExamples.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAssemblyInMVCApp1.Models;
+
+namespace WebAssemblyInMVCApp1.Controllers
+{
+    public class ComponentExamples : Controller
+    {
+        private Item item1 = new Item()
+        {
+            Id = 1,
+            Name = "Name",
+            Description = "Description",
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Server()
+        {
+            return View(item1);
+        }
+
+        public IActionResult ServerPrerendered()
+        {
+            return View(item1);
+        }
+
+        public IActionResult Static()
+        {
+            return View(item1);
+        }
+
+        public IActionResult WebAssembly()
+        {
+            return View(item1);
+        }
+
+        public IActionResult WebAssemblyPrerendered()
+        {
+            return View(item1);
+
+        }
+    }
+}
---
+++ WebAssemblyInMVCApp1/ViewComponents/ItemViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAssemblyInMVCApp1.Models;
+
+namespace WebAssemblyInMVCApp1.ViewComponents
+{
+    public class ItemViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Item itemDetails)
+        {
+            return View(itemDetails);
+        }
+    }
+}
